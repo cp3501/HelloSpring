441199fb0748a5601e11ec2565ebff0537fa83db
cp3501 <133024243+cp3501@users.noreply.github.com>
2023-11-05 15:34:13 +0000

    neu

--- hello/src/main/java/edu/fra/uas/hello/HelloApplication.java
+++ hello/src/main/java/edu/fra/uas/hello/HelloApplication.java
@@ -2,7 +2,7 @@ package edu.fra.uas.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//mein erstes Springboot-klasse
 @SpringBootApplication
 public class HelloApplication {
 
